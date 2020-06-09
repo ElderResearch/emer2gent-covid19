00e9a6375a17faf3998e9bd6dfd8e1dd9a034a4b
Andrew Stewart <andrew.stewart@elderresearch.com>
2020-06-09 16:48:31 -0400

    cleanup transform_CDC.py

--- src/python/pkg/trent/abt/transform_CDC.py
+++ src/python/pkg/trent/abt/transform_CDC.py
@@ -1,177 +1,118 @@
 #!/usr/bin/env python3
-import pandas as pd
-import numpy as np
-from datetime import datetime, timedelta
-from bs4 import BeautifulSoup
-import requests
-import math
-import io
-import os
-import re 
+from pathlib import Path
 
-""" 
+import pandas as pd
+
+"""
 Overview: Tranform CDC to common cross sectional format
 
-Centers for Disease Control and Prevention Raw scraped data 
+Centers for Disease Control and Prevention Raw scraped data
 - covid_deaths_usafacts.csv
-- covid_confirmed_usafacts.csv 
+- covid_confirmed_usafacts.csv
 
-Current Raw format 
+Current Raw format
 
-countyFIPS | County Name| State	| stateFIPS	| 1/22/20 | 1/23/20 | 1/24/20	...	
-    ...    |   ...      |  ...   |  ...     | ....    |  ...    | ... 
+countyFIPS | County Name| State	| stateFIPS	| 1/22/20 | 1/23/20 | 1/24/20	...
+    ...    |   ...      |  ...   |  ...     | ....    |  ...    | ...
 
 - Table for where rows are entire observations
 
-Output format 
+Output format
 
 deaths | date    |  county	| fips_code | state_code | confirmed
 ...    | 1/22/20 |  ...     | ....      | ...        | ...
 
-
-# UPDATE: There is a pandas implmentation to exactly this (melt)
 """
 
+data_dir = Path(__file__).resolve().parents[5] / "data"
 
+# Import data
+deaths_file = data_dir / "covid_deaths_usafacts.csv"
+confirmed_file = data_dir / "covid_confirmed_usafacts.csv"
+CDC_deaths = pd.read_csv(deaths_file)
+CDC_confirmed = pd.read_csv(confirmed_file)
 
-# ------- Helper Functions ------- #
+# Columns renamed for consistency across tables unique identifying keys
+CDC_deaths.rename(
+    inplace=True,
+    columns={
+        "countyFIPS": "county_fip",
+        "County Name": "county",
+        "State": "state_code",
+    },
+)
 
-def reformat_cdc_date(cdc_date_form):
-    """
-    Date in raw CDC takes the form month/day/year
-    To be consistent in date across tables map to year-month-day 
-    """
-    cdc_date_form += '20' # 20 -> 2020 (this works are we only collect for 2020)
-    split = cdc_date_form.split("/") #[month,day,year]
-    month = split[0]
-    day = split[1]
-    year = split[2]
-    
-    # Reformat to desired format
-    new_date = f"{year}-{month}-{day}"
-    return new_date
+CDC_confirmed.rename(
+    inplace=True,
+    columns={
+        "countyFIPS": "county_fip",
+        "County Name": "county",
+        "State": "state_code",
+    },
+)
 
-def row_to_df(row_index,CDC_df,obs_name = "deaths"):
-    """
-    Converst a row of the CDC data into desired cross sectional data 
-    obs_name will give name of the observations column eg. death or confirmed 
-    """    
+CDC_deaths.drop(columns=["stateFIPS"], inplace=True)
+CDC_confirmed.drop(columns=["stateFIPS"], inplace=True)
 
-    # Extract date columns 
-    date_columns = [k for k in CDC_df.columns if bool(re.search(r'\d', k))]
-    row_data = CDC_df.iloc[row_index,:]
-    # Get time indpendent columns 
-    row_fips_code = row_data.loc["county_fip"]
-    row_county = row_data.loc["county"]
-    row_state_code = row_data.loc["state_code"]
-    # Get observatins eg. deaths or confirmed as a list 
-    _obs = row_data.loc[date_columns].tolist() 
-    
-    # Build df: 
-    row_df = pd.DataFrame({obs_name:_obs}) # add obs. data 
-    row_df["date"] = [reformat_cdc_date(k) for k in date_columns] # add date -> reformat 
-    row_df["date"] = row_df.date.apply(lambda x: datetime.strptime(x, "%Y-%m-%d")) # convert to time stamp 
-    row_df["county"] = row_county # add county 
-    row_df["county_fip"] = row_fips_code # add county fips
-    row_df["state_code"] = row_state_code # add state code 
-    
-    return row_df   
+# Handle cases: Statewide Unallocated -> CDC has missing county level data
+# Despite having policy data present we will not have data in any other fields
+# eg. weather, ACS and so we have droped these cases
+CDC_deaths = CDC_deaths[~(CDC_deaths["county"] == "Statewide Unallocated")]
+CDC_confirmed = CDC_confirmed[~(CDC_confirmed["county"] == "Statewide Unallocated")]
 
-def to_cross_sectional_df(CDC_df,obs_name): 
-    """
-    Converst full CDC_df to cross sectonal form
-    obs_name controll if it deaths or confirmed we are looking at 
-    """    
+# New york city is present in data -> county name is new york county ->
+# remove the below as we dont have fips
+CDC_deaths = CDC_deaths[~(CDC_deaths["county"] == "New York City Unallocated/Probable")]
+CDC_confirmed = CDC_confirmed[
+    ~(CDC_confirmed["county"] == "New York City Unallocated/Probable")
+]
 
-    # List of dataframes -> to be filled 
-    list_of_df = []  
-    n_rows = len(CDC_df) 
-    # Run row_to_df over all CDC rows ie. get for all counties 
-    for i in range(n_rows):
+# Further edge cases: (no fips and not states)
+CDC_deaths = CDC_deaths[~(CDC_deaths.county == "Wade Hampton Census Area")]
+CDC_confirmed = CDC_confirmed[~(CDC_confirmed.county == "Wade Hampton Census Area")]
 
-        sub_df = row_to_df(row_index = i ,
-                           CDC_df = CDC_df,
-                           obs_name = obs_name
-                          )
-        list_of_df.append(sub_df)
-    # Join by row into one large data frame 
-    CS_df = pd.concat(list_of_df,ignore_index = True)
-    print(f"CS_df.shape = {CS_df.shape}")
-    return CS_df
+CDC_deaths = CDC_deaths[~(CDC_deaths.county == "Grand Princess Cruise Ship")]
+CDC_confirmed = CDC_confirmed[~(CDC_confirmed.county == "Grand Princess Cruise Ship")]
 
+# Get cross sectional form
+CDC_deaths_df = CDC_deaths.melt(
+    id_vars=["county_fip", "county", "state_code"],
+    var_name="date",
+    value_name="deaths",
+)
+CDC_confirmed_df = CDC_confirmed.melt(
+    id_vars=["county_fip", "county", "state_code"],
+    var_name="date",
+    value_name="confirmed",
+)
 
-if  __name__ == "__main__":
-    
-    # Import data
-    path_deaths = os.path.abspath("data/covid_deaths_usafacts.csv")
-    path_confirmed = os.path.abspath("data/covid_confirmed_usafacts.csv") 
-    CDC_deaths = pd.read_csv(path_deaths) ; print(f"CDC_deaths.shape = {CDC_deaths.shape}")
-    CDC_confirmed = pd.read_csv(path_confirmed) ; print(f"CDC_confirmed.shape = {CDC_confirmed.shape}")
+# By construction both tables match up perfectly
+# Add confirmed column to final data frame
+CDC_full_df = CDC_deaths_df.copy()
+if all(CDC_confirmed_df.date == CDC_deaths_df.date) & all(
+    CDC_confirmed_df.county_fip == CDC_deaths_df.county_fip
+):
+    print("Adding confirmed cases -------------------")
+    CDC_full_df["confirmed"] = CDC_confirmed_df["confirmed"]
+else:
+    print("Data not matched")
 
-    # Columns renamed for consistency across tables unique idetifying keys 
-    CDC_deaths.rename(
-                    inplace = True , 
-                    columns = {
-                                "countyFIPS":"county_fip",
-                                "County Name": "county",
-                                "State": "state_code"
-                            }
-                    ) 
+# Reorder columns:
+new_column_order = [
+    "date",
+    "county_fip",
+    "state_code",
+    "county",
+    "deaths",
+    "confirmed",
+]
+CDC_full_df = CDC_full_df.loc[:, new_column_order]
+CDC_full_df["date"] = pd.to_datetime(CDC_full_df["date"])
 
-    CDC_confirmed.rename(
-                    inplace = True , 
-                    columns = {
-                                "countyFIPS":"county_fip",
-                                "County Name": "county",
-                                "State": "state_code"
-                            }
-                    ) 
+# Export: Check that intermediate data folder exisits O.W create
+intermediate_dir = data_dir / "intermediate"
+if not intermediate_dir.exists():
+    intermediate_dir.mkdir()
 
-    
-    # Handel cases: Statewide Unallocated -> CDC has missing county level data 
-    # Despite having policy data present we will not have data in any other fields eg. weather, ACS and so we have droped these cases
-    CDC_deaths = CDC_deaths[~(CDC_deaths["county"] == "Statewide Unallocated")]
-    CDC_confirmed = CDC_confirmed[~(CDC_confirmed["county"] == "Statewide Unallocated")]
-
-    # New york city is present in data -> county name is new york county -> remove the bellow as we dont have fips 
-    CDC_deaths = CDC_deaths[~(CDC_deaths["county"] == "New York City Unallocated/Probable")]
-    CDC_confirmed = CDC_confirmed[~(CDC_confirmed["county"] == "New York City Unallocated/Probable")]
-
-    # Further edge cases: (no fips and not states)
-    CDC_deaths = CDC_deaths[~(CDC_deaths.county == "Wade Hampton Census Area")]
-    CDC_confirmed = CDC_confirmed[~(CDC_confirmed.county == "Wade Hampton Census Area")]
-
-    CDC_deaths = CDC_deaths[~(CDC_deaths.county == "Grand Princess Cruise Ship")]
-    CDC_confirmed = CDC_confirmed[~(CDC_confirmed.county == "Grand Princess Cruise Ship")]
-
-
-    print(f"CDC_deaths.shape = {CDC_deaths.shape}")
-    print(f"CDC_confirmed.shape = {CDC_confirmed.shape}")
-
-    # Get cross sectiona form: 
-    CDC_deaths_df = to_cross_sectional_df(CDC_df = CDC_deaths,obs_name="deaths")
-    CDC_confirmed_df = to_cross_sectional_df(CDC_df = CDC_confirmed,obs_name="confirmed")
-
-    # By construction both tables match up perfectly
-    # Add confirmed colums to fina data frame 
-    CDC_full_df = CDC_deaths_df.copy()
-    if all(CDC_confirmed_df.date == CDC_deaths_df.date) & all(CDC_confirmed_df.county_fip == CDC_deaths_df.county_fip):
-        print("Adding confirmed cases -------------------")
-        CDC_full_df["confirmed"] = CDC_confirmed_df["confirmed"]
-    else:
-         print("Data not matched")
-
-    # Reorder columns: 
-    new_column_order = ["date","county_fip","state_code","county","deaths","confirmed"]
-    CDC_full_df = CDC_full_df.loc[:,new_column_order]
-
-
-    # Export: Check that intermediate data folder exisits O.W create 
-    check_directory = os.path.abspath("data/intermediate")
-    print(check_directory)
-    if not os.path.exists(check_directory):
-        os.makedirs(check_directory)
-
-    # Export CDC_full_df to csv 
-    file_name = "CDC_full_data.csv"
-    CDC_full_df.to_csv(f"{check_directory}/{file_name}",index = False)
+# Export CDC_full_df to csv
+CDC_full_df.to_csv(intermediate_dir / "CDC_full_data.csv", index=False)
